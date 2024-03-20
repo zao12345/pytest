e150cc2a9882d5e1c5f74183d21b32d5da63d946
Zainab Abdussalam-Osimade <74374137+zao12345@users.noreply.github.com>
2024-03-20 19:20:28 -0400

    Update test_area.py
    Changed the failed test to a correct value

--- tests/test_area.py
+++ tests/test_area.py
@@ -17,6 +17,6 @@ def test_calculate_area_square_list():
 
 def test_calculate_area_square_correct():
     # Given
-    side_length = 5
+    side_length = math.sqrt(33)
     area = calculate_area_square(side_length)
     assert area == 33
